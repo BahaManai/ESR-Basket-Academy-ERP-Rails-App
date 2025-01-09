3d1f76c7a170beb8711fa4aba77321abda2b2ca6
Baha Manai <bahaeddinmanai7@gmail.com>
2025-01-09 17:11:40 +0100

    programmation des états des paiements et de joueurs (Expiré,Active,Crédit,Non crédit

--- app/models/paiement.rb
+++ app/models/paiement.rb
@@ -1,7 +1,7 @@
 class Paiement < ApplicationRecord
   belongs_to :joueur
   validates :etat_abonnement, inclusion: {
-    in: [ "Active", "Expiré", "Crédit" ],
+    in: [ "Non crédit", "Crédit" ],
     message: "%{value} n'est pas un état valide"
   }
 end
