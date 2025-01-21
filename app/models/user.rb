c3a9abdc6fbfcbacbe3abc2da6c502b22b13cce0
Baha Manai <bahaeddinmanai7@gmail.com>
2025-01-21 11:35:54 +0100

    Développer le navbar de titre + compte utilisateur / Logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :role, presence: true, inclusion: { in: [ "Administrateur", "Opérateur" ], message: "%{value} n'est pas un rôle valide" }
 end
