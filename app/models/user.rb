238c241895c812e7fdfba6306ed77ead77fc80f3
Baha Manai <bahaeddinmanai7@gmail.com>
2025-01-22 09:39:41 +0100

    Ajouter les validations coté serveur et coté client pour Utilisateurs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :role, presence: true, inclusion: { in: [ "Administrateur", "Opérateur" ], message: "%{value} n'est pas un rôle valide" }
+  validates :role, inclusion: { in: [ "Administrateur", "Opérateur" ], message: "%{value} n'est pas un rôle valide" }
+  validates :user_name, presence: true, uniqueness: { case_sensitive: false }, format: {
+    with: /\A[a-zA-Z][a-zA-Z0-9_]*\z/,
+    message: "ne peut contenir que des lettres, des chiffres et des underscores (_) et doit commencer par une lettre." }
 end
