e28b8a7df6f2b20a4985060e17706e085bd32530
Baha Manai <bahaeddinmanai7@gmail.com>
2025-02-03 16:58:17 +0100

    Final app deseign part 1

--- db/migrate/20250203124052_add_foreign_keys.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeys < ActiveRecord::Migration[8.0]
-  def change
-    add_foreign_key :achats, :joueurs
-    add_foreign_key :assurances, :joueurs
-    add_foreign_key :assurances, :saisons
-  end
-end
